adcb429b99abe2682aad0b1b0027339b854040d2
Joey Cadieux <instructor.joeycadieux@gmail.com>
2023-06-11 15:40:03 -0400

    first commit

---
+++ techtack-landing/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <NavbarModule />
+    <div>
+      <ContainerCards />
+    </div>
+    <FooterModule />
+  </div>
+</template>
+
+<script setup>
+import NavbarModule from '../components/NavbarModule';
+import FooterModule from '../components/FooterModule';
+import ContainerCards from '../components/containerCards';
+</script>
